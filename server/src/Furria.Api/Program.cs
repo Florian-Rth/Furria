2f5394f62260006f63b6e5d8ea5725cadcae2b24
Florian-Rth <19978683+Florian-Rth@users.noreply.github.com>
2026-07-19 15:04:54 +0200

    feat: expose swagger/openapi docs for the api

--- server/src/Furria.Api/Program.cs
+++ server/src/Furria.Api/Program.cs
@@ -1,4 +1,5 @@
 using FastEndpoints;
+using FastEndpoints.Swagger;
 using Furria.Application;
 using Furria.Infrastructure;
 
@@ -7,10 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument(o =>
+{
+    o.DocumentSettings = s =>
+    {
+        s.Title = "Furria API";
+        s.Version = "v1";
+    };
+});
 
 var app = builder.Build();
 
 app.UseFastEndpoints(c => c.Endpoints.RoutePrefix = "api");
+app.UseSwaggerGen();
 
 app.Run();
 
