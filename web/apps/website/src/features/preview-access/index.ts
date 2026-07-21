5be14f455b6eb78e1898984a726de6d935b854c7
Florian-Rth <flori2232@gmail.com>
2026-07-21 19:38:41 +0200

    fix(website): order preview-access barrel exports

--- web/apps/website/src/features/preview-access/index.ts
+++ web/apps/website/src/features/preview-access/index.ts
@@ -1,5 +1,5 @@
 export { PreviewAccessDialog } from './components/PreviewAccessDialog';
-export { PreviewTeaser } from './components/PreviewTeaser/PreviewTeaser';
 export { PreviewAccessProvider } from './components/PreviewAccessProvider';
+export { PreviewTeaser } from './components/PreviewTeaser/PreviewTeaser';
 export { usePreviewAccess } from './hooks/use-preview-access';
 export { readGrantedFromSession, writeGrantedToSession } from './session-storage';
